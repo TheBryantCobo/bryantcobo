06afb926c423a266d9d6ff0aa4bc1be994aed000
Bryant Cobarrubias <me@bryantcobo.com>
2022-05-17 00:58:13 -0500

    File cleanup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Social from './components/Social';
-// import Footer from './components/Footer';
 
 const App = () => {
   return (
